32658f58369857b68b058c61decf31791e399b27
Daniel Mindru <daniel.mindru@endava.com>
2024-03-06 17:21:11 +0200

    Create Request and Response Objects for data manipulation

---
+++ src/test/java/ObjectData/RequestPreparation.java
@@ -0,0 +1,9 @@
+package ObjectData;
+
+import java.util.HashMap;
+
+public interface RequestPreparation {
+
+    // Aceasta interfata are ca scop sa serializeze un specific request body
+    void prepareObject(HashMap<String, String> testData);
+}
