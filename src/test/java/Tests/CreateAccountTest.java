d98ab5209fbe7f39c1aa67c26f6c690ae5244711
Daniel Mindru <daniel.mindru@endava.com>
2024-04-19 17:20:45 +0300

    Add Reporting with Extent Reports and Hooks for Test Suite

--- src/test/java/Tests/CreateAccountTest.java
+++ src/test/java/Tests/CreateAccountTest.java
@@ -6,13 +6,16 @@ import ObjectData.ResponseObject.ResponseAccountGetSuccess;
 import ObjectData.ResponseObject.ResponseAccountSuccess;
 import ObjectData.ResponseObject.ResponseTokenSuccess;
 import PropertyUtility.PropertyUtility;
+import extentUtility.ExtentUtility;
+import extentUtility.ReportStep;
+import hooks.Hooks;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class CreateAccountTest {
+public class CreateAccountTest extends Hooks {
     public String userId;
     public String token;
     public RequestAccount requestAccountBody;
@@ -22,22 +25,23 @@ public class CreateAccountTest {
     public void testMethod() {
         System.out.println("Step 1: Create new account");
         createAccount();
-        System.out.println();
+        ExtentUtility.attachReportLog(ReportStep.PASS_STEP, "The user creates a new account with success");
 
         System.out.println("Step 2: Generate new token");
         generateToken();
-        System.out.println();
+        ExtentUtility.attachReportLog(ReportStep.PASS_STEP, "The user generates a token for new account with success");
 
         System.out.println("Step 3: Get new account");
         getSpecificAccount();
-        System.out.println();
+        ExtentUtility.attachReportLog(ReportStep.PASS_STEP, "The user validates the presence of new account with success");
 
         System.out.println("Step 4: Delete new account");
         deleteSpecificAccount();
+        ExtentUtility.attachReportLog(ReportStep.PASS_STEP, "The user deletes the new account with success");
 
         System.out.println("Step 5: Re-check deleted account");
         getSpecificAccount();
-        System.out.println();
+        ExtentUtility.attachReportLog(ReportStep.PASS_STEP, "The user validates the presence of deleted account with success");
     }
 
     public void createAccount() {
